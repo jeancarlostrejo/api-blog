8e8a2dfca3af3ca950f8693b5f5b2cb68e4bef32
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-18 01:39:30 -0400

    add create and index post method with formRequest and relationship beetwen user and post model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,25 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
 use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+use function PHPUnit\Framework\isEmpty;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        $posts = Post::all();
+
+        if(isEmpty($posts)){
+            return response()->json(['message' => 'No posts yet', 'data' => $posts], 200);
+        }
+
+        return response()->json(['data' => $posts]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request): JsonResponse
     {
-        //
+        $validated = $request->validated();
+
+        $validated["image"] = Storage::put('posts', $validated["image"]);
+
+        $post = auth()->user()->posts()->create($validated);
+
+        return response()->json(['message' => 'Post created successfully', 'data' => $post], 201);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,9 @@ Route::controller(AuthController::class)->group(function () {
     Route::post('/register',  'register')->name('register');
     Route::post('/login',  'login')->name('login');
     Route::post('/logout',  'logout')->middleware('auth:sanctum')->name('logout');
+});
+
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/posts', PostController::class);
 });
