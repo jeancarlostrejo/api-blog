d7919b8843f6f439d7f383318f60f36617e500d1
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-22 09:31:00 -0400

    add validations for subscribe and un unsubscribe to users

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -10,15 +10,27 @@ class SubscriptionController extends Controller
 {
     public function subscribe(User $user): JsonResponse
     {
+        if(auth()->user()->id === $user->id){
+            return response()->json(['message' => "You can't subscribe yourself"], 422);
+        }
+
+        if(auth()->user()->subscriptions->contains($user)){
+            return response()->json(['message' => "You cannot subscribe to a user you are already subscribed"], 422);
+        }
+
         auth()->user()->subscriptions()->attach($user);
 
         return response()->json(['message' => 'Subscribed satisfully']);
     }
 
-    public function unsubscribe(User $user)
+    public function unsubscribe(User $user): JsonResponse
     {
+        if(!(auth()->user()->subscriptions->contains($user))){
+            return response()->json(['message' => "You are not subscribed to this user"], 422);
+        }
+
         auth()->user()->subscriptions()->detach($user);
 
-        return response()->json(['message' => 'Unsusbcribe satisfully']);
+        return response()->json(['message' => 'Unsubscribe satisfully']);
     }
 }
