e1951a9a4b12d5f9d3a996e3e41f5e7729c53167
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-10-22 08:16:32 -0400

    add migrations and relations for subscriptions of users with other users

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -57,5 +58,17 @@ class User extends Authenticatable
     public function likes(): HasMany
     {
         return $this->hasMany(Like::class);
+    }
+
+    //Retorna todos los usuarios que estan suscritos al usuario actual
+    public function subscribers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'subscriptions', 'subscribed_to_id', 'subscriber_id');
+    }
+
+    //Retorna todos los usuarios a los que el usuario actual está subscrito.
+    public function subscriptions(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'subscriptions', 'subscriober_id', 'subscribed_to_id');
     }
 }
